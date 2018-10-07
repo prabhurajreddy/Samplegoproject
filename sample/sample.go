bdc16718f73887c4d4378260f913589ad154cd47
prabhu raj <prabhuraj@prabhus-MacBook-Air.local>
2018-10-07 14:52:50 +0800

    Initial commit

---
+++ sample/sample.go
@@ -0,0 +1,9 @@
+// Package sample provides a simple function to print statement
+package sample
+
+import "fmt"
+
+// PrintSample function just prints the statement
+func PrintSample() {
+	fmt.Println("Print sample package")
+}
